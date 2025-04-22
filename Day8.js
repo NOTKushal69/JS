005d0a3d63625f1a2d2891bc337559a89dfb8dab
NOTKushal69 <156913735+NOTKushal69@users.noreply.github.com>
2025-04-22 20:58:42 +0545

    added new file

---
+++ Day8.js
@@ -0,0 +1,25 @@
+// hame jav ek variale mein ek le jyada value rakhne heh ko array bana sakte ho
+// array=[1,2,3,4,5]
+
+
+
+arrebhai=[1,2,3,4,5];
+
+
+const hoe=arrebhai.filter((nu)=>{
+return nu >= 3;
+})
+
+console.log(hoe)
+
+
+
+
+
+
+// const matwnaya =arrebhai.map((bachha)=>{
+//   return {
+//     Number:bachha
+//   }
+// })
+// console.log(matwnaya)
