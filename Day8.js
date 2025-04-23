229c11c45110bb8b3a5b6f9ce519ea702ddeafa9
NOTKushal69 <156913735+NOTKushal69@users.noreply.github.com>
2025-04-23 06:03:14 +0545

    Modification

--- Day8.js
+++ Day8.js
@@ -3,23 +3,43 @@
 
 
 
-arrebhai=[1,2,3,4,5];
 
 
-const hoe=arrebhai.filter((nu)=>{
-return nu >= 3;
-})
+// const hoe=arrebhai.filter((nu)=>{
+  // return nu >= 3;
+  // })
+  
+  // console.log(hoe)
+  
 
-console.log(hoe)
+  // // const matwnaya =arrebhai.map((bachha)=>{
+    // //   return {
+      // //     Number:bachha
+      // //   }
+      // // })
+      // // console.log(matwnaya)
+      
+//       // abui=
+//           arrebhai=[1,2,3,4,5];
+// console.log(arrebhai.indexOf(2))
+
+
+//       // y=arrebhai.find((r)=>{
+//       //   if (r==2) {return r}
+      // })
+      // console.log(y)
 
 
 
 
+obj={
+  name:"Kushal",
+  age:16
+};
+// 
+Object.freeze(obj)
+// console.log(typeof(Object.keys(obj)));
+obj['name']="Bishal";
 
 
-// const matwnaya =arrebhai.map((bachha)=>{
-//   return {
-//     Number:bachha
-//   }
-// })
-// console.log(matwnaya)
+console.log(obj);
